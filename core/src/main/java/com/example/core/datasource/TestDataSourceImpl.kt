2e51c70baabbecf56d6a72315c52adc2f7f3bbc0
lyh990517 <lyh990517@naver.com>
2024-01-01 00:09:57 +0900

    [Core] Add TestDataSource

---
+++ core/src/main/java/com/example/core/datasource/TestDataSourceImpl.kt
@@ -0,0 +1,12 @@
+package com.example.core.datasource
+
+import com.example.core.TestMapper
+import com.example.core.model.TestUiModel
+import com.example.network_api.model.TestResponse
+import com.example.network_api.repository.TestRepository
+import javax.inject.Inject
+
+class TestDataSourceImpl @Inject constructor(private val testRepository: TestRepository) :
+    TestDataSource {
+    override suspend fun getResponse(): TestUiModel = TestMapper.mapToUiModel(testRepository.getTestResponse())
+}
---
+++ core/src/main/java/com/example/core/model/TestUiModel.kt
@@ -0,0 +1,5 @@
+package com.example.core.model
+
+data class TestUiModel(
+    val data : String
+)
