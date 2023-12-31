efdea64604464c6a8fdb96ce908140c54ed695dd
lyh990517 <lyh990517@naver.com>
2024-01-01 00:12:03 +0900

    [App] Set HiltApplication

--- app/src/main/java/com/example/footballmanager_pj/MainActivity.kt
+++ app/src/main/java/com/example/footballmanager_pj/MainActivity.kt
@@ -1,18 +1,25 @@
-package com.example.footballmanager_pj.ui
+package com.example.footballmanager_pj
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.footballmanager_pj.ui.theme.Footballmanager_pjTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +29,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Scaffold {
+                        FootBallManagerAppNavigator()
+                    }
                 }
             }
         }
