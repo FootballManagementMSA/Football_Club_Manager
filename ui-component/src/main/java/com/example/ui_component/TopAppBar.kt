36aedced7bc22a4a37df2a5cf0124ed59a80ce07
lyh990517 <lyh990517@naver.com>
2024-01-02 00:13:11 +0900

    [Util] Edit TopAppBar

--- ui-component/src/main/java/com/example/ui_component/TopAppBar.kt
+++ ui-component/src/main/java/com/example/ui_component/TopAppBar.kt
@@ -6,9 +6,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -30,28 +32,30 @@ fun TopAppBar(
             .fillMaxWidth()
             .wrapContentHeight(),
         horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.Bottom
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Icon(
-            modifier = modifier
-                .size(bigIcon)
-                .clickable {
-                    onBack()
-                },
-            imageVector = Icons.Default.ArrowBack,
-            contentDescription = "NavIcon",
-            tint = Color.White
-        )
+        IconButton(
+            modifier = modifier.wrapContentSize(),
+            onClick = onBack
+        ){
+            Icon(
+                modifier = modifier.size(bigIcon) ,
+                imageVector = Icons.Default.ArrowBack,
+                contentDescription = "NavIcon",
+                tint = Color.White
+            )
+        }
         Text(text = title, style = TextStyle(color = Color.White), fontSize = appBarFont)
-        Icon(
-            modifier = modifier
-                .size(bigIcon)
-                .clickable {
-                    onAction()
-                },
-            imageVector = actionIcon,
-            contentDescription = "Action",
-            tint = Color.White
-        )
+        IconButton(
+            modifier = modifier.wrapContentSize(),
+            onClick = onAction
+        ){
+            Icon(
+                modifier = modifier.size(bigIcon) ,
+                imageVector = actionIcon,
+                contentDescription = "Action",
+                tint = Color.White
+            )
+        }
     }
 }
