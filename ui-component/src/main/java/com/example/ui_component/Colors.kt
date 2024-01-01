46442ee38141cc426493630e96fbcd37cbf24bfd
lyh990517 <lyh990517@naver.com>
2024-01-02 00:05:08 +0900

    [Util] Add Constants

---
+++ ui-component/src/main/java/com/example/ui_component/Colors.kt
@@ -0,0 +1,5 @@
+package com.example.ui_component
+
+import androidx.compose.ui.graphics.Color
+
+val mainTheme = Color(0xFF232A39)
---
+++ ui-component/src/main/java/com/example/ui_component/Size.kt
@@ -0,0 +1,10 @@
+package com.example.ui_component
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+val bigIcon = 24.dp
+val middleIcon = 20.dp
+val smallIcon = 16.dp
+
+val appBarFont = 18.sp
