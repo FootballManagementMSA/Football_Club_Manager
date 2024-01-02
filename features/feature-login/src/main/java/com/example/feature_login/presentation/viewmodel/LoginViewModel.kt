db21b3dbfc49229326c2ff22914d20478a2f3b68
임성우 <seongwoorang@naver.com>
2024-01-02 21:38:37 +0900

    [Feature-login] Add viewmodel

---
+++ features/feature-login/src/main/java/com/example/feature_login/presentation/viewmodel/LoginViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.feature_login.presentation.viewmodel
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.feature_login.domain.usecase.LoginUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val loginUseCase: LoginUseCase
+) : ViewModel() {
+    private val _userId = mutableStateOf("")
+    val userId: State<String> = _userId
+
+    private val _userPassword = mutableStateOf("")
+    val userPassword: State<String> = _userPassword
+
+    fun login() {
+        loginUseCase
+    }
+
+    fun updateUserId(userId: String) {
+        _userId.value = userId
+    }
+
+    fun updateUserPassword(userPassword: String) {
+        _userPassword.value = userPassword
+    }
+
+}
