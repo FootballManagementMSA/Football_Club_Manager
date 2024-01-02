1c162f70abe55c1db56c60273c405cc158664f47
lyh990517 <lyh990517@naver.com>
2024-01-02 15:12:56 +0900

    [Feature-Home] Add InfoRow

--- features/feature-home/src/main/java/com/example/presentation/screen/HomeScreen.kt
+++ features/feature-home/src/main/java/com/example/presentation/screen/HomeScreen.kt
@@ -3,17 +3,21 @@ package com.example.presentation.screen
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Text
@@ -34,11 +38,14 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.ui_component.CircleShapeClickableIcon
 import com.example.ui_component.HorizontalSpacer
+import com.example.ui_component.InfoItem
 import com.example.ui_component.R
 import com.example.ui_component.TopAppBar
 import com.example.ui_component.VerticalSpacer
 import com.example.ui_component.bigFont
+import com.example.ui_component.grayText
 import com.example.ui_component.hugeFont
+import com.example.ui_component.infoRow
 import com.example.ui_component.largeIcon
 import com.example.ui_component.mainTheme
 import com.example.ui_component.mediumIcon
@@ -76,8 +83,48 @@ fun HomeScreen(
                 )
             )
             ProfileView(height, width)
+            Spacer(
+                modifier = Modifier.height(
+                    (height / 20).coerceAtLeast(30.dp).coerceAtMost(60.dp)
+                )
+            )
+            MyInfoView(height)
         }
     }
+}
+
+@Composable
+fun MyInfoView(height: Dp) {
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(16.dp))
+            .height((height / 10).coerceAtLeast(80.dp))
+            .background(infoRow),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        InfoItem(title = "나이", content = "25")
+        VerticalLine()
+        InfoItem(title = "게임 수", content = "70")
+        VerticalLine()
+        InfoItem(title = "골 수", content = "35")
+        VerticalLine()
+        InfoItem(title = "주 포메이션", content = "NF")
+        VerticalLine()
+        InfoItem(title = "주발", content = "L")
+    }
+}
+
+@Composable
+fun VerticalLine() {
+    Box(
+        Modifier
+            .fillMaxHeight()
+            .padding(vertical = 16.dp)
+            .width(1.dp)
+            .background(grayText)
+    )
 }
 
 @Composable
