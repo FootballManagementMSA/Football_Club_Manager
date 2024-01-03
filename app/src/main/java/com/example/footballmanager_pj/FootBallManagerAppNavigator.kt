eae3c9e699faee8138090bd158b188a1f418ad0a
lyh990517 <lyh990517@naver.com>
2024-01-03 14:16:05 +0900

    [App] Set Bottom Navigation to Scaffold

--- app/src/main/java/com/example/footballmanager_pj/FootBallManagerAppNavigator.kt
+++ app/src/main/java/com/example/footballmanager_pj/FootBallManagerAppNavigator.kt
@@ -1,19 +1,27 @@
 package com.example.footballmanager_pj
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.feature_navigation.Route
 import com.example.presentation.screen.HomeScreen
 
 @Composable
 fun FootBallManagerAppNavigator(
-    navHostController: NavHostController = rememberNavController()
+    navHostController: NavHostController
 ) {
-    NavHost(navController = navHostController, startDestination = "HOME"){
-        composable("HOME"){
+    NavHost(navController = navHostController, startDestination = Route.HOME) {
+        composable(Route.HOME) {
             HomeScreen()
+        }
+        composable(Route.SCHEDULE) {
+            Text(text = "SCHEDULE")
+        }
+        composable(Route.SETTINGS) {
+            Text(text = "SETTINGS")
         }
     }
 }
