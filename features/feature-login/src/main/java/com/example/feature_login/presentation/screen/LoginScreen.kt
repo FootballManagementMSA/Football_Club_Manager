2a7fdc0351eb7a6dd8cbde127e6f1961c90bca67
임성우 <seongwoorang@naver.com>
2024-01-03 19:18:13 +0900

    [Feature-login] Add LoginScreen Preview

--- features/feature-login/src/main/java/com/example/feature_login/presentation/screen/LoginScreen.kt
+++ features/feature-login/src/main/java/com/example/feature_login/presentation/screen/LoginScreen.kt
@@ -11,9 +11,15 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ProvidableCompositionLocal
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -24,12 +30,82 @@ import com.example.ui_component.CustomGradientButton
 import com.example.ui_component.InputView
 import com.example.ui_component.WhiteButton
 
+val LocalLoginScreenPreviewMode: ProvidableCompositionLocal<Boolean> = compositionLocalOf { false }
 
 @Composable
 fun LoginScreen(
-    viewModel: LoginViewModel = hiltViewModel()
 ) {
+    if (LocalLoginScreenPreviewMode.current) {
+        PreviewLoginScreen()
+    } else {
+        val viewModel: LoginViewModel = hiltViewModel()
+        val scrollState = rememberScrollState()
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(scrollState)
+                .background(color = Color(0xFF232A39))
+                .padding(40.dp)
+        ) {
+            IconView()
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            Text(
+                text = "안녕하세요.\n\n앱이름 입니다.",
+                fontSize = 30.sp,
+                color = Color.White,
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            InputView(
+                state = viewModel.userId,
+                label = " 학번",
+                placeholder = "학번을 입력해 주세요.",
+                onValueChange = { viewModel.updateUserId(it) })
+
+            Spacer(modifier = Modifier.height(15.dp))
+
+            InputView(
+                state = viewModel.userPassword,
+                label = "비밀번호",
+                placeholder = "비밀번호를 입력해 주세요",
+                onValueChange = { viewModel.updateUserPassword(it) })
+
+            Spacer(modifier = Modifier.height(35.dp))
+
+            CustomGradientButton(
+                gradientColors = listOf(Color(0xFFEE6DE7), Color(0xFF857FEB)),
+                cornerRadius = 16.dp,
+                buttonText = "로그인",
+                roundedCornerShape = RoundedCornerShape(20.dp)
+            ) {
+                viewModel.login()
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            LoginClickableTextView()
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            WhiteButton(
+                buttonText = "회원가입",
+                textColor = Color(0xFF857FEB),
+                roundedCornerShape = RoundedCornerShape(20.dp)
+            ) {
+            }
+        }
+    }
+
+}
+
+@Composable
+fun PreviewLoginScreen() {
     val scrollState = rememberScrollState()
+    val state = remember { mutableStateOf("preview") }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -51,18 +127,18 @@ fun LoginScreen(
         Spacer(modifier = Modifier.height(20.dp))
 
         InputView(
-            state = viewModel.userId,
+            state = state,
             label = " 학번",
             placeholder = "학번을 입력해 주세요.",
-            onValueChange = { viewModel.updateUserId(it) })
+            onValueChange = { })
 
         Spacer(modifier = Modifier.height(15.dp))
 
         InputView(
-            state = viewModel.userPassword,
+            state = state,
             label = "비밀번호",
             placeholder = "비밀번호를 입력해 주세요",
-            onValueChange = { viewModel.updateUserPassword(it) })
+            onValueChange = { })
 
         Spacer(modifier = Modifier.height(35.dp))
 
@@ -72,7 +148,6 @@ fun LoginScreen(
             buttonText = "로그인",
             roundedCornerShape = RoundedCornerShape(20.dp)
         ) {
-            viewModel.login()
         }
 
         Spacer(modifier = Modifier.height(16.dp))
@@ -87,5 +162,13 @@ fun LoginScreen(
             roundedCornerShape = RoundedCornerShape(20.dp)
         ) {
         }
+    }
+}
+
+@Preview
+@Composable
+fun LoginScreenPreview() {
+    CompositionLocalProvider(LocalLoginScreenPreviewMode provides true) {
+        LoginScreen()
     }
 }
