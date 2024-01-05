14ea89f19422614ce03e35a62d05979522336d9b
lyh990517 <lyh990517@naver.com>
2024-01-05 09:14:50 +0900

    [App] Fix Conflict Error

--- app/src/main/java/com/example/footballmanager_pj/FootBallManagerAppNavigator.kt
+++ app/src/main/java/com/example/footballmanager_pj/FootBallManagerAppNavigator.kt
@@ -5,7 +5,6 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.example.feature_navigation.Route
 import com.example.presentation.screen.HomeScreen
 
@@ -15,7 +14,7 @@ fun FootBallManagerAppNavigator(
 ) {
     NavHost(navController = navHostController, startDestination = Route.HOME) {
         composable(Route.HOME) {
-            HomeScreen()
+            HomeScreen(navHostController)
         }
         composable(Route.SCHEDULE) {
             Text(text = "SCHEDULE")
