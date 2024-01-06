8ceed231e9eb0bcc2f8a83123d2c97d49f95bd91
임성우 <seongwoorang@naver.com>
2024-01-06 17:38:54 +0900

    [Feature-MakeClub] Add MakeClubTopView

---
+++ features/feature-makeclub/src/main/java/com/example/feature_makeclub/presentation/ui_component/MakeClubTopView.kt
@@ -0,0 +1,83 @@
+package com.example.feature_makeclub.presentation.ui_component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.ui_component.R
+import com.example.ui_component.VerticalSpacer
+import com.example.ui_component.values.largeIcon
+@Composable
+fun MakeClubTopView() {
+    Column {
+        NextArrowImageView()
+        VerticalSpacer(value = 48)
+        TermsAgreeText()
+        VerticalSpacer(value = 70)
+    }
+}
+
+//@Composable
+//fun NextArrowImageView() {
+//    Column {
+//        Box(
+//            modifier = Modifier
+//                .align(Alignment.CenterHorizontally)
+//        ) {
+//            Icon(
+//                painter = painterResource(id = R.drawable.next_icon),
+//                contentDescription = null,
+//                modifier = Modifier
+//                    .size(largeIcon)
+//                    .align(Alignment.TopEnd),
+//                tint = Color.White
+//            )
+//        }
+//    }
+//}
+
+@Composable
+fun NextArrowImageView() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.next_icon),
+            contentDescription = null,
+            modifier = Modifier
+                .size(largeIcon)
+                .align(Alignment.TopEnd),
+            tint = Color.White
+        )
+    }
+}
+
+
+@Composable
+fun TermsAgreeText() {
+    Text(
+        text = "구단의 이름을\n\n입력해 주세요.",
+        fontSize = 30.sp,
+        color = Color.White,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Composable
+@Preview
+fun TopScreenPreview() {
+    MakeClubTopView()
+}
