25d41c42735d689bacc31934c2b23699201e5719
임성우 <seongwoorang@naver.com>
2024-01-06 17:43:23 +0900

    [Feature-MakeClub] Add ClubInfoBox

---
+++ features/feature-makeclub/src/main/java/com/example/feature_makeclub/presentation/ui_component/ClubIdInfoBox.kt
@@ -0,0 +1,63 @@
+package com.example.feature_makeclub.presentation.ui_component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.ui_component.HorizontalSpacer
+import com.example.ui_component.R
+import com.example.ui_component.VerticalSpacer
+import com.example.ui_component.values.lastGradientColor
+import com.example.ui_component.values.tinyFont
+
+@Composable
+fun ClubIdInfoBox(text: String) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .padding(horizontal = 10.dp)
+            .fillMaxWidth(),
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .size(20.dp)
+                .clickable {}
+        )
+        {
+            Image(
+                painter = painterResource(id = R.drawable.check_icon),
+                contentDescription = "CheckIcon",
+                modifier = Modifier.size(12.dp)
+            )
+        }
+        HorizontalSpacer(value = 5)
+        Text(
+            text = text,
+            color = lastGradientColor,
+            maxLines = 1,
+            style = TextStyle(
+                fontSize = tinyFont
+            )
+        )
+
+    }
+    VerticalSpacer(value = 110)
+}
+
+@Composable
+@Preview
+fun ClubIdInfoBoxPreview() {
+    ClubIdInfoBox("preview")
+}
