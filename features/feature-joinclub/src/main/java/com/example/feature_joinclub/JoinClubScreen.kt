42ae33333aae2956b7d84b0944313a7f4622a81d
lyh990517 <lyh990517@naver.com>
2024-01-07 21:59:24 +0900

    [Feature-JoinClub] Add Temp BottomSheet

--- features/feature-joinclub/src/main/java/com/example/feature_joinclub/JoinClubScreen.kt
+++
@@ -1,8 +0,0 @@
-package com.example.feature_joinclub
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun JoinClubScreen() {
-
-}
---
+++ features/feature-joinclub/src/main/java/com/example/feature_joinclub/presentation/screen/JoinClubScreen.kt
@@ -0,0 +1,55 @@
+package com.example.feature_joinclub.presentation.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun JoinClubScreen() {
+    val showSheet = remember {
+        mutableStateOf(false)
+    }
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        if (showSheet.value) {
+            BottomSheet {
+                showSheet.value = false
+            }
+        }
+        Button(onClick = {
+            showSheet.value = !showSheet.value
+        }) {
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomSheet(onDismiss: () -> Unit) {
+    val modalBottomSheetState = rememberModalBottomSheetState()
+
+    ModalBottomSheet(
+        modifier = Modifier.height(500.dp),
+        onDismissRequest = { onDismiss() },
+        sheetState = modalBottomSheetState,
+        dragHandle = { BottomSheetDefaults.DragHandle() },
+    ) {
+        Text(text = "hi")
+    }
+}
