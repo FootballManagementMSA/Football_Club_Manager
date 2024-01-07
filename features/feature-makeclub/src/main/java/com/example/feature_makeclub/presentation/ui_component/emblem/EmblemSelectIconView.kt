34b78997c5a9712432ef4a814af043e34b32df72
임성우 <seongwoorang@naver.com>
2024-01-07 20:18:07 +0900

    [Feature-MakeClub] Add image selecting feature

--- features/feature-makeclub/src/main/java/com/example/feature_makeclub/presentation/ui_component/emblem/EmblemSelectIconView.kt
+++ features/feature-makeclub/src/main/java/com/example/feature_makeclub/presentation/ui_component/emblem/EmblemSelectIconView.kt
@@ -1,34 +1,68 @@
 package com.example.feature_makeclub.presentation.ui_component.emblem
 
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
+import com.example.feature_makeclub.presentation.viewmodel.MakeClubViewModel
 import com.example.ui_component.R
 
 @Composable
-fun EmblemSelectIconView() {
+fun EmblemSelectIconView(viewModel: MakeClubViewModel = hiltViewModel()) {
+    val selectedImageUri = viewModel.selectedImageUri.collectAsState()
+    val singlePhotoPickerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia(),
+        onResult = { uri ->
+            viewModel.updateSelectedImageUri(uri)
+        }
+    )
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier
             .size(100.dp)
-            .clickable { }
+            .clickable {
+                singlePhotoPickerLauncher.launch(
+                    PickVisualMediaRequest(
+                        ActivityResultContracts.PickVisualMedia.ImageOnly
+                    )
+                )
+            }
     )
     {
-        Image(
-            painter = painterResource(id = R.drawable.check_icon),
-            contentDescription = "CheckIcon",
-            modifier = Modifier.size(100.dp)
-        )
+        if (selectedImageUri.value == null) {
+            Image(
+                painter = painterResource(id = R.drawable.check_icon),
+                contentDescription = "CheckIcon",
+                modifier = Modifier.size(100.dp)
+            )
+        } else {
+            AsyncImage(
+                model = selectedImageUri.value,
+                contentDescription = null,
+                placeholder = painterResource(R.drawable.back_icon),
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
+            )
+        }
     }
 }
-
 @Preview
 @Composable
 fun EmblemSelectIconViewPreview() {
--- features/feature-makeclub/src/main/java/com/example/feature_makeclub/presentation/viewmodel/MakeClubViewModel.kt
+++ features/feature-makeclub/src/main/java/com/example/feature_makeclub/presentation/viewmodel/MakeClubViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.feature_makeclub.presentation.viewmodel
 
+import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -12,9 +13,16 @@ class MakeClubViewModel @Inject constructor(
     private val _clubName = MutableStateFlow("")
     val clubName: StateFlow<String> = _clubName
 
+    private  val _selectedImageUri = MutableStateFlow<Uri?>(null)
+    val selectedImageUri: StateFlow<Uri?> = _selectedImageUri
+
     fun updateClubName(clubName: String) {
         viewModelScope.launch {
             _clubName.emit(clubName)
         }
+    }
+
+    fun updateSelectedImageUri(uri: Uri?) {
+        _selectedImageUri.value = uri
     }
 }
