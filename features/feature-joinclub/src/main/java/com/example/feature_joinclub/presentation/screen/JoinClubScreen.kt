6b29f4b4908598f186dfa611197ba21827bdab01
lyh990517 <lyh990517@naver.com>
2024-01-07 22:59:15 +0900

    [Feature-JoinClub] Add BottomSheet TeamList

--- features/feature-joinclub/src/main/java/com/example/feature_joinclub/presentation/screen/JoinClubScreen.kt
+++ features/feature-joinclub/src/main/java/com/example/feature_joinclub/presentation/screen/JoinClubScreen.kt
@@ -1,21 +1,36 @@
 package com.example.feature_joinclub.presentation.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.core.model.Club
+import com.example.core.model.ClubUiModel
+import com.example.ui_component.DefaultItem
+import com.example.ui_component.DefaultListView
+import com.example.ui_component.values.mainTheme
+import com.example.ui_component.values.verticalGradation
 
 @Composable
 fun JoinClubScreen() {
@@ -23,7 +38,9 @@ fun JoinClubScreen() {
         mutableStateOf(false)
     }
     Column(
-        Modifier.fillMaxSize(),
+        Modifier
+            .fillMaxSize()
+            .background(mainTheme),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -42,14 +59,90 @@ fun JoinClubScreen() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BottomSheet(onDismiss: () -> Unit) {
+    val config = LocalConfiguration.current
     val modalBottomSheetState = rememberModalBottomSheetState()
-
+    val teamList = remember {
+        mutableStateOf(
+            dummyClub()
+        )
+    }
     ModalBottomSheet(
-        modifier = Modifier.height(500.dp),
+        modifier = Modifier.height(config.screenHeightDp.dp),
         onDismissRequest = { onDismiss() },
         sheetState = modalBottomSheetState,
         dragHandle = { BottomSheetDefaults.DragHandle() },
     ) {
-        Text(text = "hi")
+        DefaultListView(
+            modifier = Modifier.padding(20.dp),
+            themeColor = Color.Black,
+            listName = "현재 가입된 구단 목록",
+            listIcon = Icons.Default.Person,
+            buttonName = "전체보기",
+            showButton = true,
+            onClick = { },
+            listContent = {
+                items(teamList.value.club, key = { it.name }) {
+                    DefaultItem(
+                        modifier = Modifier
+                            .padding(bottom = 12.dp),
+                        radius = 8.dp,
+                        color = Color.White,
+                        clickable = true
+                    ) {
+
+                    }
+                }
+            }
+        )
     }
 }
+
+@Composable
+@Preview
+fun JoinClubScreenPreview() {
+    JoinClubScreen()
+}
+
+private fun dummyClub() = ClubUiModel(
+    club =
+    listOf(
+        Club(
+            name = "a", "14",
+            "hello"
+        ),
+        Club(
+            name = "b", "18",
+            "world"
+        ), Club(
+            name = "c", "18",
+            "world"
+        ),
+        Club(
+            name = "d", "18",
+            "world"
+        ),
+        Club(
+            name = "e", "18",
+            "world"
+        ),
+        Club(
+            name = "f", "14",
+            "hello"
+        ),
+        Club(
+            name = "g", "18",
+            "world"
+        ), Club(
+            name = "h", "18",
+            "world"
+        ),
+        Club(
+            name = "i", "18",
+            "world"
+        ),
+        Club(
+            name = "j", "18",
+            "world"
+        )
+    )
+)
