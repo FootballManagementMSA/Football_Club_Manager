65ee962a2af510499a571c8db3bde2d08eb56a43
lyh990517 <lyh990517@naver.com>
2024-01-08 23:16:23 +0900

    [Util] Add UI Parameter

--- features/feature-home/src/main/java/com/example/presentation/ui_component/ScheduleView.kt
+++ features/feature-home/src/main/java/com/example/presentation/ui_component/ScheduleView.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -26,6 +27,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -127,6 +129,7 @@ fun ScheduleViewPreview() {
 
 @Composable
 fun EmptyScheduleContent() {
+    val config = LocalConfiguration.current
     Box(modifier = Modifier.fillMaxSize()) {
         Box(
             modifier = Modifier
@@ -148,7 +151,11 @@ fun EmptyScheduleContent() {
                 fontWeight = FontWeight.Bold
             )
             VerticalSpacer(value = 14)
-            RoundedIconButton(200.dp) {}
+            RoundedIconButton(
+                modifier = Modifier.width(config.screenWidthDp.dp / 2),
+                icon = Icons.Default.DateRange,
+                content = "일정 생성"
+            ) {}
         }
     }
 }
