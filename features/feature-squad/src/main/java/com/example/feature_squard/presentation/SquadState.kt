d26374a11aef3d7301169fb80ddb4319e37780d8
lyh990517 <lyh990517@naver.com>
2024-01-12 14:26:36 +0900

    [Feature-Squad] Fix Data Type

--- features/feature-squad/src/main/java/com/example/feature_squard/presentation/SquadState.kt
+++ features/feature-squad/src/main/java/com/example/feature_squard/presentation/SquadState.kt
@@ -1,10 +1,11 @@
 package com.example.feature_squard.presentation
 
+import com.example.core.model.Position
 import com.example.core.model.PositionPreset
 
 sealed class SquadState {
 
     data object Loading : SquadState()
 
-    data class Success(val data: PositionPreset) : SquadState()
+    data class Success(val data: Position) : SquadState()
 }
--- features/feature-squad/src/main/java/com/example/feature_squard/presentation/screen/SquadScreen.kt
+++ features/feature-squad/src/main/java/com/example/feature_squard/presentation/screen/SquadScreen.kt
@@ -1,16 +1,22 @@
 package com.example.feature_squard.presentation.screen
 
-import android.content.res.Configuration
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
@@ -18,9 +24,9 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.core.model.Position
 import com.example.feature_squard.presentation.SquadState
 import com.example.feature_squard.presentation.viewmodel.SquadViewModel
-import kotlin.math.roundToInt
 import com.example.ui_component.R
 import com.example.ui_component.values.mainTheme
+import kotlin.math.roundToInt
 
 @Composable
 fun SquadScreen(viewModel: SquadViewModel = hiltViewModel()) {
@@ -35,7 +41,7 @@ fun SquadScreen(viewModel: SquadViewModel = hiltViewModel()) {
 
         is SquadState.Success -> {
             DraggableBox(
-                onLoad = { (state as SquadState.Success).data.user1 },
+                onLoad = { (state as SquadState.Success).data },
                 onSet = { position ->
                     Log.e("callback","$position")
                     viewModel.savePosition(position)
