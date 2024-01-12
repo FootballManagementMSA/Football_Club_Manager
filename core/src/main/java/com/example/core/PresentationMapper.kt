0b3f917b9ae85b93c4a68c4f070b8094a4d3be39
lyh990517 <lyh990517@naver.com>
2024-01-12 15:07:41 +0900

    [Core] Edit Mapper

---
+++ core/src/main/java/com/example/core/PresentationMapper.kt
@@ -0,0 +1,26 @@
+package com.example.core
+
+import com.example.core.model.Position
+import com.example.core.model.Screen
+import com.example.core.model.TestUiModel
+import com.example.network_api.model.PositionPreset
+import com.example.network_api.model.TestResponse
+
+object PresentationMapper {
+    fun mapToUiModel(testResponse: TestResponse) = TestUiModel(testResponse.data)
+
+    fun mapToUiPreset(positionPreset: PositionPreset) =
+        com.example.core.model.PositionPreset(
+            localScreen = DataMapper.mapToDataModel(positionPreset.screenSize),
+            user1 = DataMapper.mapToDataModel2(positionPreset.user1)
+        )
+}
+
+object DataMapper {
+
+    fun mapToDataModel(screen: com.example.network_api.model.Screen) =
+        Screen(screen.width, screen.height)
+
+    fun mapToDataModel2(position: com.example.network_api.model.Position) =
+        Position(position.x, position.y)
+}
