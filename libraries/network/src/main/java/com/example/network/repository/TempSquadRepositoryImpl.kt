ad248891fd2ab6f8f49b89691e1db4cadd4bfe93
lyh990517 <lyh990517@naver.com>
2024-01-13 20:07:27 +0900

    [Data] Change Fixed Value To Flexible value

--- libraries/network/src/main/java/com/example/network/repository/TempSquadRepositoryImpl.kt
+++ libraries/network/src/main/java/com/example/network/repository/TempSquadRepositoryImpl.kt
@@ -8,13 +8,15 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
 //추후 API로 대체
-internal class TempSquadRepositoryImpl @Inject constructor() : TempSquadRepository {
+internal class TempSquadRepositoryImpl @Inject constructor(
+    private val screen: Screen
+) : TempSquadRepository {
     private val tempPositionStore = MutableStateFlow(Position(500f, 300f))
 
     //스크린 크기는 nexus 4 임시로 설정
     override suspend fun loadMyCustomSquadPreset(): PositionPreset {
         return PositionPreset(
-            screenSize = Screen(768.0, 1280.0),
+            screenSize = screen, //기기 변동이 있을 경우를 대비하여 DB에 저장된 스크린 사이즈 필요
             user1 = Position(tempPositionStore.value.x, tempPositionStore.value.y)
         )
     }
