0ce8c1436ae942dfd67fb517029e48866c913490
lyh990517 <lyh990517@naver.com>
2024-01-13 13:57:22 +0900

    [Core] Remove Useless Code

--- core/src/main/java/com/example/core/model/PositionPreset.kt
+++ core/src/main/java/com/example/core/model/PositionPreset.kt
@@ -1,12 +1,6 @@
 package com.example.core.model
 
 data class PositionPreset(
-    val localScreen: Screen,
+    val screenSize: Screen,
     val user1 : Position
-) {
-    companion object {
-        fun PositionPreset.mapToPresentation(){
-
-        }
-    }
-}
+)
