f694caaf637ce270b1dc5f4db4cfc008c5578200
lyh990517 <lyh990517@naver.com>
2024-01-13 20:07:59 +0900

    [Feature-Squad] Change Success Data

--- features/feature-squad/src/main/java/com/example/feature_squard/presentation/SquadState.kt
+++ features/feature-squad/src/main/java/com/example/feature_squard/presentation/SquadState.kt
@@ -7,5 +7,5 @@ sealed class SquadState {
 
     data object Loading : SquadState()
 
-    data class Success(val data: Position) : SquadState()
+    data class Success(val data: PositionPreset) : SquadState()
 }
