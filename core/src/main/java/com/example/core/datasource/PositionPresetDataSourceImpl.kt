04846cd939e531fd5a52dbcd9f4cd9d9b7ece135
lyh990517 <lyh990517@naver.com>
2024-01-13 13:55:04 +0900

    [Core] Trim DataSource

--- core/src/main/java/com/example/core/datasource/PositionPresetDataSourceImpl.kt
+++ core/src/main/java/com/example/core/datasource/PositionPresetDataSourceImpl.kt
@@ -20,21 +20,7 @@ import javax.inject.Singleton
 @Singleton
 class PositionPresetDataSourceImpl @Inject constructor(private val tempSquadRepository: TempSquadRepository) :
     PositionPresetDataSource {
-//    private val localScreen = MutableStateFlow(Screen(0.0, 0.0))
-//    private val remoteScreen = MutableStateFlow(Screen(768.0, 1280.0)) // 상대의 휴대폰 화면 사이즈
-//    private val positionStore = MutableStateFlow(Position(400f, 400f))
 
-//    init {
-//        val manager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
-//        val (w, h) = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-//            manager.currentWindowMetrics.bounds.width() to manager.currentWindowMetrics.bounds.height()
-//        } else {
-//            manager.defaultDisplay.width to manager.defaultDisplay.height
-//        }
-//        localScreen.value = Screen(w.toDouble(), h.toDouble())
-//    }
-
-    //나의 커스텀 프리셋을 저장할 때
     override suspend fun save(localScreen: Screen, position: Position) {
         tempSquadRepository.saveMyCustomSquadPreset(
             position = position.mapToDataModel(), screen = localScreen.mapToDataModel()
@@ -45,12 +31,7 @@ class PositionPresetDataSourceImpl @Inject constructor(private val tempSquadRepo
         return PresentationMapper.mapToUiPreset(tempSquadRepository.loadMyCustomSquadPreset())
     }
 
-    //나 또는 상대의 커스텀 프리셋을 불러올 때
     override suspend fun loadOtherUserPreset(): PositionPreset {
-//        val pos = Position(
-//            x = positionStore.value.x * (localScreen.value.width / remoteScreen.value.width).toFloat(),
-//            y = positionStore.value.y * (localScreen.value.height / remoteScreen.value.height).toFloat()
-//        )
         return PresentationMapper.mapToUiPreset(tempSquadRepository.loadOtherUserCustomSquadPreset())
     }
 }
