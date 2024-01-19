2ca65aeb15fe08be81410811680c7cfd9923ed37
임성우 <seongwoorang@naver.com>
2024-01-19 16:52:47 +0900

    [Feature-MyPage] Modify MyPageModifyScreen to use weight

--- features/feature-mypage/src/main/java/com/example/feature_mypage/presentation/screen/MyPageModifyScreen.kt
+++ features/feature-mypage/src/main/java/com/example/feature_mypage/presentation/screen/MyPageModifyScreen.kt
@@ -1,56 +1,74 @@
 package com.example.feature_mypage.presentation.screen
 
-import android.net.Uri
+import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.requiredHeightIn
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.feature_mypage.presentation.ui_component.ModifyProfileImageView
 import com.example.feature_mypage.presentation.ui_component.MyPageModifyInfoView
-import com.example.feature_mypage.presentation.ui_component.MyPageTopView
 import com.example.ui_component.CustomGradientButton
-import com.example.ui_component.DefaultEmblemSelectIconView
-import com.example.ui_component.VerticalSpacer
 import com.example.ui_component.values.gradientColorsList
 import com.example.ui_component.values.mainTheme
 
 @Composable
 fun MyPageModifyScreen() {
-    val state = remember { mutableStateOf<Uri?>(null) }
     val scrollState = rememberScrollState()
-
+    val config = LocalConfiguration.current
     Column(
-        modifier = Modifier
-            .background(mainTheme)
-            .fillMaxSize()
-            .verticalScroll(scrollState)
+        if (isScrollable(config))
+            Modifier
+        else
+            Modifier.verticalScroll(scrollState)
     ) {
-        MyPageTopView(title = "마이페이지", onBack = {}) {
-        }
-
-        DefaultEmblemSelectIconView(
-            modifier = Modifier.fillMaxWidth(),
-            state = state,
-            onSelect = {})
-        MyPageModifyInfoView(modifier = Modifier.fillMaxWidth())
-        CustomGradientButton(
-            gradientColors = gradientColorsList,
-            cornerRadius = 16.dp,
-            buttonText = "확인",
-            roundedCornerShape = RoundedCornerShape(20.dp)
+        Column(
+            if (isScrollable(config))
+                Modifier
+                    .background(mainTheme)
+            else
+                Modifier
+                    .requiredHeightIn(950.dp)
+                    .background(mainTheme)
         ) {
+            ModifyProfileImageView(modifier = Modifier
+                .requiredHeightIn(min = 200.dp)
+                .weight(2f))
+            MyPageModifyInfoView(
+                modifier = Modifier
+                    .requiredHeightIn(min = 650.dp)
+                    .weight(8f)
+                    .fillMaxWidth()
+            )
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .requiredHeightIn(100.dp)
+                    .align(Alignment.CenterHorizontally)
+                    .width(300.dp)
+            ) {
+                CustomGradientButton(
+                    gradientColors = gradientColorsList,
+                    cornerRadius = 16.dp,
+                    buttonText = "확인",
+                    roundedCornerShape = RoundedCornerShape(20.dp)
+                ) {
+                }
+            }
         }
-        VerticalSpacer(value = 55)
     }
 }
+
+private fun isScrollable(config: Configuration) = config.screenHeightDp.dp > 950.dp
 
 @Preview
 @Composable
