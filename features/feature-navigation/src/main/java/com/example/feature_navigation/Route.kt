fef54b05ad1e57e7a965af70b40d23dabbff0c3f
임성우 <seongwoorang@naver.com>
2024-01-19 16:55:16 +0900

    [Feature-MyPage] Add Route

--- features/feature-navigation/src/main/java/com/example/feature_navigation/Route.kt
+++ features/feature-navigation/src/main/java/com/example/feature_navigation/Route.kt
@@ -7,4 +7,6 @@ object Route {
     const val MAKE_CLUB = "MAKE_CLUB"
     const val EMBLEM_SELECT = "EMBLEM_SELECT"
     const val COMPLETE_CLUB_MAKING = "COMPLETE_CLUB_MAKING"
+    const val MYPAGE = "MYPAGE"
+    const val MYPAGE_MODIFY = "MYPAGE_MODIFY"
 }
