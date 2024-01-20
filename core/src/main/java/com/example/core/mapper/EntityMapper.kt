707719db8acf62f0df847f2e871942d857115d17
lyh990517 <lyh990517@naver.com>
2024-01-20 22:42:07 +0900

    [Core] Edit Mapper

--- core/src/main/java/com/example/core/mapper/EntityMapper.kt
+++ core/src/main/java/com/example/core/mapper/EntityMapper.kt
@@ -1,9 +1,11 @@
 package com.example.core.mapper
 
+import com.example.core.mapper.EntityMapper.mapToEntity
 import com.example.core.model.LocalScreen
 import com.example.core.model.PositionPresetUIModel
-import com.example.core.model.PositionUiModel
-import com.example.network_api.entity.Position
+import com.example.core.model.MemberUiModel
+import com.example.core.model.Position
+import com.example.network_api.entity.Member
 import com.example.network_api.entity.PositionPreset
 import com.example.network_api.entity.RemoteScreen
 
@@ -11,10 +13,21 @@ object EntityMapper {
     fun PositionPresetUIModel.mapToEntity() =
         PositionPreset(
             screenSize = this.screenSize.mapToEntity(),
-            memberPosition = this.memberPosition.map { it.mapToEntity() }
+            members = this.members.map { it.mapToEntity() }
         )
 
-    fun PositionUiModel.mapToEntity() = Position(this.x, this.y)
+    fun MemberUiModel.mapToEntity() = Member(
+        id = this.id,
+        name = this.name,
+        role = this.role,
+        number = this.number,
+        position = this.position.mapToEntity()
+    )
+
+    fun Position.mapToEntity() = com.example.network_api.entity.Position(
+        x = this.x,
+        y = this.y
+    )
 
     fun LocalScreen.mapToEntity() = RemoteScreen(this.width, this.height)
 }
