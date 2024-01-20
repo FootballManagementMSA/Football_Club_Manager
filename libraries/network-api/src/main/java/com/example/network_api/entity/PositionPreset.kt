3094ae4754e43aae008c86c89961bcd2f99e18b1
lyh990517 <lyh990517@naver.com>
2024-01-20 22:40:38 +0900

    [Data] Add Member Model

--- libraries/network-api/src/main/java/com/example/network_api/entity/PositionPreset.kt
+++ libraries/network-api/src/main/java/com/example/network_api/entity/PositionPreset.kt
@@ -2,5 +2,5 @@ package com.example.network_api.entity
 
 data class PositionPreset(
     val screenSize: RemoteScreen = RemoteScreen(0.0, 0.0),
-    val memberPosition: List<Position>
+    val members: List<Member>
 )
