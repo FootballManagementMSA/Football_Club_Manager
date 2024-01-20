766b9a45626bd8937750e71dbd049609fa1d25c9
lyh990517 <lyh990517@naver.com>
2024-01-20 22:44:34 +0900

    [Feature-Squad] Add User Name

--- features/feature-squad/src/main/java/com/example/feature_squard/presentation/ui_component/DraggableMember.kt
+++ features/feature-squad/src/main/java/com/example/feature_squard/presentation/ui_component/DraggableMember.kt
@@ -2,59 +2,76 @@ package com.example.feature_squard.presentation.ui_component
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import com.example.core.model.PositionPresetUIModel
-import com.example.core.model.PositionUiModel
+import com.example.core.model.LocalScreen
+import com.example.core.model.MemberUiModel
+import com.example.core.model.Position
 import com.example.ui_component.R
 import kotlin.math.roundToInt
 
 @Composable
 fun DraggableMember(
-    memberNumber: Int,
-    onLoad: () -> PositionPresetUIModel,
-    onSet: (PositionUiModel) -> Unit
+    onLoad: () -> Pair<MemberUiModel,LocalScreen>,
+    onSet: (MemberUiModel) -> Unit
 ) {
-    val loaded = remember { mutableStateOf(onLoad().memberPosition[memberNumber]) }
-    val screenSize by remember { mutableStateOf(onLoad().screenSize) }
-    val boxSize = remember { mutableStateOf(20) }
-    Image(
-        modifier = Modifier
+    var member by remember { mutableStateOf(onLoad().first) }
+    val screenSize by remember { mutableStateOf(onLoad().second) }
+    val draggableModifier = remember {
+        Modifier
             .offset {
                 IntOffset(
-                    loaded.value.x
+                    member.position.x
                         .roundToInt()
                         .coerceAtLeast(0)
-                        .coerceAtMost(screenSize.width.toInt() - boxSize.value * 2),
-                    loaded.value.y
+                        .coerceAtMost(screenSize.width.toInt()),
+                    member.position.y
                         .roundToInt()
                         .coerceAtLeast(0)
                         .coerceAtMost(screenSize.height.toInt()),
                 )
             }
-            .size(boxSize.value.dp)
             .pointerInput(Unit) {
                 detectDragGestures { change, dragAmount ->
                     change.consume()
-                    loaded.value = loaded.value.copy(
-                        x = (loaded.value.x + dragAmount.x)
-                            .coerceAtLeast(0f)
-                            .coerceAtMost(screenSize.width.toFloat()),
-                        y = (loaded.value.y + dragAmount.y)
-                            .coerceAtLeast(0f)
-                            .coerceAtMost(screenSize.height.toFloat()),
+                    member = member.copy(
+                        position = Position(
+                            x = (member.position.x + dragAmount.x)
+                                .coerceAtLeast(0f)
+                                .coerceAtMost(screenSize.width.toFloat()),
+                            y = (member.position.y + dragAmount.y)
+                                .coerceAtLeast(0f)
+                                .coerceAtMost(screenSize.height.toFloat())
+                        )
                     )
-                    onSet(loaded.value)
+                    onSet(member)
                 }
-            }, painter = painterResource(id = R.drawable.cloth_icon), contentDescription = "member"
-    )
+            }
+    }
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(
+            modifier = draggableModifier.size(20.dp),
+            painter = painterResource(id = R.drawable.cloth_icon),
+            contentDescription = member.name
+        )
+        Spacer(modifier = draggableModifier.padding(vertical = 3.dp))
+        Text(modifier = draggableModifier.wrapContentSize(), text = member.name, color = Color.White)
+    }
 }
