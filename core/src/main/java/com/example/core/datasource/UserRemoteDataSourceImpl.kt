98f4d6e574e51b4d36c85beb45ee7d40a8cb3749
임성우 <seongwoorang@naver.com>
2024-01-22 17:06:30 +0900

    [Core] Add suspend func in user remote datasource

--- core/src/main/java/com/example/core/datasource/UserRemoteDataSourceImpl.kt
+++ core/src/main/java/com/example/core/datasource/UserRemoteDataSourceImpl.kt
@@ -1,12 +1,17 @@
 package com.example.core.datasource
 
+import com.example.core.model.UserInfo
 import javax.inject.Inject
 
 internal class UserRemoteDataSourceImpl @Inject constructor(
-): UserRemoteDataSource {
+) : UserRemoteDataSource {
     override suspend fun login() {
     }
 
     override suspend fun sendClubInfoData() {
+    }
+
+    override suspend fun getJoinClubUserInfo(): List<UserInfo> {
+        return listOf(UserInfo("13", "3", "st", "erer"))
     }
 }
