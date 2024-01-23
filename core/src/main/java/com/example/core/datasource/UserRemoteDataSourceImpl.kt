44e2fe3c74fec2269cd7896b2b1a7a45b33912a2
임성우 <seongwoorang@naver.com>
2024-01-23 17:06:34 +0900

    [Core] Add name parameter in dummy data

--- core/src/main/java/com/example/core/datasource/UserRemoteDataSourceImpl.kt
+++ core/src/main/java/com/example/core/datasource/UserRemoteDataSourceImpl.kt
@@ -10,8 +10,8 @@ internal class UserRemoteDataSourceImpl @Inject constructor(
 
     override suspend fun sendClubInfoData() {
     }
-
+    //레포지토리에서 가져오도록 수정
     override suspend fun getJoinClubUserInfo(): List<UserInfo> {
-        return listOf(UserInfo("13", "3", "st", "erer"))
+        return listOf(UserInfo("임성우","133333", "3", "st", "erer"), UserInfo("테스트","26","5","GK","안녕하세요 테스트입니다."))
     }
 }
