3c513b093834e42b90fc64c157550803cddac9ce
lyh990517 <lyh990517@naver.com>
2024-01-24 01:33:01 +0900

    [Util-Calendar] Add CalendarControlViewPreview

--- util/calendar/src/main/java/com/example/calendar/CalendarControlView.kt
+++ util/calendar/src/main/java/com/example/calendar/CalendarControlView.kt
@@ -3,7 +3,9 @@ package com.example.calendar
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
@@ -12,23 +14,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.ui_component.HorizontalSpacer
-import com.example.ui_component.VerticalSpacer
-import com.example.ui_component.values.bigFont
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun CalendarControlView(
+    modifier: Modifier = Modifier,
     year: Int,
     month: Int,
     pagerState: PagerState,
     pageCount: Int
 ) {
     val coroutineScope = rememberCoroutineScope()
-    Text(text = "날짜 선택", fontSize = bigFont)
-    VerticalSpacer(value = 10)
-    Row {
+    Row(modifier) {
         Text(text = "$year 년 $month 월")
         HorizontalSpacer(value = 10)
         Icon(
@@ -44,11 +44,22 @@ fun CalendarControlView(
         Icon(
             modifier = Modifier.clickable {
                 coroutineScope.launch {
-                    pagerState.animateScrollToPage((pagerState.currentPage + 1).coerceAtMost(pageCount))
+                    pagerState.animateScrollToPage(
+                        (pagerState.currentPage + 1).coerceAtMost(
+                            pageCount
+                        )
+                    )
                 }
             },
             imageVector = Icons.AutoMirrored.Default.KeyboardArrowRight,
             contentDescription = "key"
         )
     }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Preview
+@Composable
+fun CalendarControlView() {
+    CalendarControlView(modifier = Modifier.fillMaxWidth(), year = 2024, month = 1, pagerState = rememberPagerState { 5 }, pageCount = 5)
 }
