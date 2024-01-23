f3c576380de57c53390e097f7abb6f1895e55f7a
lyh990517 <lyh990517@naver.com>
2024-01-23 14:33:15 +0900

    [Util-Calendar] Make CalendarView

--- ui-component/src/main/java/com/example/ui_component/Calendar.kt
+++ ui-component/src/main/java/com/example/ui_component/Calendar.kt
@@ -2,87 +2,142 @@ package com.example.ui_component
 
 import android.os.Build
 import android.util.Log
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.ui_component.values.verticalGradation
-import java.time.LocalDate
 import java.time.YearMonth
 import java.time.format.TextStyle
 import java.time.temporal.TemporalAdjusters
+import java.util.Calendar
 import java.util.Locale
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HorizontalScrollCalendar() {
     val page = rememberPagerState { 5 }
-    val days = make(2024, 2)  // 2024년 1월에 대한 일자와 요일 리스트
-    days.forEach { week ->
-        week.forEach { day ->
-            if (day != null) {
-                Log.e("123","${day.day}(${day.dayOfWeek}) ")
-            } else {
-                Log.e("123","   ")
-            }
-        }
-        println()
-    }
+    val days = makeCalendar(2024, 2)
     HorizontalPager(
-        modifier = Modifier
-            .background(verticalGradation), state = page
+        modifier = Modifier.fillMaxSize(), state = page
     ) {
-        Column(
-            Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Bottom
-        ) {
-            Row(
-                Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
+        CalendarView(days)
+    }
+}
 
-            }
+@Composable
+private fun CalendarView(days: List<List<CalendarDate?>>) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.padding(20.dp)) {
+        days.forEach { week ->
+            WeekRow(week)
+            Spacer(modifier = Modifier.weight(1f))
         }
     }
 }
 
-data class Day(
+@Composable
+private fun WeekRow(week: List<CalendarDate?>) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        week.forEach { day ->
+            CalendarItem(day)
+        }
+    }
+}
+
+@Composable
+private fun CalendarItem(day: CalendarDate?) {
+    Box(modifier = Modifier.size(20.dp)) {
+        Text(modifier = Modifier.align(Alignment.Center), text = "${day?.day}")
+    }
+}
+
+data class CalendarDate(
     val year: Int,
     val month: Int,
     val day: Int,
     val dayOfWeek: String
 )
 
+fun makeCalendar(year: Int, month: Int): List<List<CalendarDate?>> {
+    val daysList = MutableList(5) { MutableList<CalendarDate?>(7) { null } }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val firstDayOfMonth = YearMonth.of(year, month).atDay(1)
+        var currentDay =
+            firstDayOfMonth.with(TemporalAdjusters.previousOrSame(java.time.DayOfWeek.SUNDAY))
 
-@RequiresApi(Build.VERSION_CODES.O)
-fun make(year: Int, month: Int): List<List<Day?>> {
-    val daysList = MutableList(5) { MutableList<Day?>(7) { null } } // 6행 7열 리스트 초기화
-
-    val firstDayOfMonth = YearMonth.of(year, month).atDay(1)
-    var currentDay = firstDayOfMonth.with(TemporalAdjusters.previousOrSame(java.time.DayOfWeek.SUNDAY))
-
-    for (week in 0 until 5) {
-        for (dayOfWeek in 0 until 7) {
-            val adjustedYear = if (month == 1 && currentDay.monthValue == 12) year - 1 else if (month == 12 && currentDay.monthValue == 1) year + 1 else year
-            if (currentDay.monthValue == month || currentDay.isBefore(firstDayOfMonth) || currentDay.monthValue == (month % 12) + 1) {
-                val dayName = currentDay.dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault())
-                daysList[week][dayOfWeek] = Day(adjustedYear, currentDay.monthValue, currentDay.dayOfMonth, dayName)
+        for (week in 0 until 5) {
+            for (dayOfWeek in 0 until 7) {
+                val adjustedYear =
+                    if (month == 1 && currentDay.monthValue == 12) year - 1 else if (month == 12 && currentDay.monthValue == 1) year + 1 else year
+                if (currentDay.monthValue == month || currentDay.isBefore(firstDayOfMonth) || currentDay.monthValue == (month % 12) + 1) {
+                    val dayName =
+                        currentDay.dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault())
+                    daysList[week][dayOfWeek] =
+                        CalendarDate(
+                            adjustedYear,
+                            currentDay.monthValue,
+                            currentDay.dayOfMonth,
+                            dayName
+                        )
+                }
+                currentDay = currentDay.plusDays(1)
             }
-            currentDay = currentDay.plusDays(1)
+        }
+    } else {
+        val calendar = Calendar.getInstance().apply {
+            set(Calendar.YEAR, year)
+            set(Calendar.MONTH, month - 1)
+            set(Calendar.DAY_OF_MONTH, 1)
+        }
+
+        val firstDayOfMonth = calendar.get(Calendar.DAY_OF_WEEK)
+        calendar.add(Calendar.DATE, -firstDayOfMonth + Calendar.SUNDAY)
+
+        for (week in 0 until 5) {
+            for (dayOfWeek in 0 until 7) {
+                val adjustedYear = calendar.get(Calendar.YEAR)
+                val adjustedMonth =
+                    calendar.get(Calendar.MONTH) + 1
+                val dayName =
+                    calendar.getDisplayName(
+                        Calendar.DAY_OF_WEEK,
+                        Calendar.SHORT,
+                        Locale.getDefault()
+                    )
+
+                if (adjustedMonth == month || calendar.before(Calendar.getInstance().apply {
+                        set(Calendar.YEAR, year)
+                        set(Calendar.MONTH, month - 1)
+                        set(Calendar.DAY_OF_MONTH, 1)
+                    }) || adjustedMonth == (month % 12) + 1) {
+                    daysList[week][dayOfWeek] = CalendarDate(
+                        adjustedYear,
+                        adjustedMonth,
+                        calendar.get(Calendar.DAY_OF_MONTH),
+                        dayName ?: ""
+                    )
+                }
+
+                calendar.add(Calendar.DATE, 1)
+            }
         }
     }
 
