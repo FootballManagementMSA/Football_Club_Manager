91858d6591fb1b7f9d35745bbe773b2cf0a3d7c1
lyh990517 <lyh990517@naver.com>
2024-01-24 15:29:35 +0900

    [Util-Calendar] Add Calendar ViewModel

--- util/calendar/src/main/java/com/example/calendar/Calendar.kt
+++ util/calendar/src/main/java/com/example/calendar/Calendar.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -16,8 +18,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.calendar.model.CalendarDate
 import com.example.calendar.util.CalendarUtil.makeCalenderPage
+import com.example.calendar.viewmodel.CalendarViewModel
 import com.example.ui_component.DefaultRoundedButton
 import com.example.ui_component.VerticalSpacer
 import com.example.ui_component.values.bigFont
@@ -27,47 +31,60 @@ import com.example.ui_component.values.darkButton
 @Composable
 fun Calendar(
     modifier: Modifier = Modifier,
+    viewModel: CalendarViewModel = hiltViewModel(),
     pageCount: Int = 12,
     onSelect: (CalendarDate) -> Unit
 ) {
     val context = LocalContext.current
     val pagerState = rememberPagerState { pageCount }
-    val selectedIndex = remember { mutableStateOf(Triple(-1, -1, -1)) }
-    val calendarPages = remember { makeCalenderPage(pageCount)}
-    Column(
-        modifier
-            .background(Color.White)
-    ) {
-        Text(text = "날짜 선택", fontSize = bigFont)
-        VerticalSpacer(value = 10)
-        CalendarControlView(
-            Modifier,
-            calendarPages[pagerState.currentPage].year,
-            calendarPages[pagerState.currentPage].month,
-            pagerState,
-            pageCount
-        )
-        VerticalSpacer(value = 10)
-        HorizontalPager(
-            modifier = modifier.weight(1f), state = pagerState
-        ) { monthIndex ->
-            Month(
-                calendarPages[monthIndex].calendar,
-                selectedIndex,
-                monthIndex
-            )
+    val uiState = viewModel.uiState.collectAsState()
+
+    LaunchedEffect(Unit) {
+        viewModel.loadCalendar(pageCount)
+    }
+    when (uiState.value) {
+        CalendarState.Loading -> {
+
         }
-        DefaultRoundedButton(
-            modifier = Modifier,
-            cornerRadius = 32.dp,
-            buttonText = "선택",
-            buttonColor = darkButton
-        ) {
-            if (selectedIndex.value == Triple(-1, -1, -1)) {
-                Toast.makeText(context, "날짜를 선택 해 주세요", Toast.LENGTH_SHORT).show()
-            } else {
-                val (month, week, day) = selectedIndex.value
-                onSelect(calendarPages[month].calendar[day][week]!!)
+
+        is CalendarState.Success -> {
+            val calendarPages = remember { (uiState.value as CalendarState.Success).calendarPage }
+            Column(
+                modifier
+                    .background(Color.White)
+            ) {
+                Text(text = "날짜 선택", fontSize = bigFont)
+                VerticalSpacer(value = 10)
+                CalendarControlView(
+                    Modifier,
+                    calendarPages[pagerState.currentPage].year,
+                    calendarPages[pagerState.currentPage].month,
+                    pagerState,
+                    pageCount
+                )
+                VerticalSpacer(value = 10)
+                HorizontalPager(
+                    modifier = modifier.weight(1f), state = pagerState
+                ) { monthIndex ->
+                    Month(
+                        calendarPages[monthIndex].calendar,
+                        viewModel.selectedIndex,
+                        monthIndex
+                    )
+                }
+                DefaultRoundedButton(
+                    modifier = Modifier,
+                    cornerRadius = 32.dp,
+                    buttonText = "선택",
+                    buttonColor = darkButton
+                ) {
+                    if (viewModel.selectedIndex.value == Triple(-1, -1, -1)) {
+                        Toast.makeText(context, "날짜를 선택 해 주세요", Toast.LENGTH_SHORT).show()
+                    } else {
+                        val (month, week, day) = viewModel.selectedIndex.value
+                        onSelect(calendarPages[month].calendar[day][week]!!)
+                    }
+                }
             }
         }
     }
