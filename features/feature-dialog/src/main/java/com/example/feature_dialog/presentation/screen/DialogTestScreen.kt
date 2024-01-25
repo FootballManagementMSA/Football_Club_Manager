69c2815f2fcd996fca7843be0482c6eb7b59a2a2
임성우 <seongwoorang@naver.com>
2024-01-25 16:12:35 +0900

    [Feature-Dialog] Modify To Using Sealed Class

--- features/feature-dialog/src/main/java/com/example/feature_dialog/presentation/screen/DialogTestScreen.kt
+++ features/feature-dialog/src/main/java/com/example/feature_dialog/presentation/screen/DialogTestScreen.kt
@@ -3,6 +3,7 @@ package com.example.feature_dialog.presentation.screen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -12,25 +13,44 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.feature_dialog.presentation.DialogState
 import com.example.feature_dialog.presentation.viewmodel.DialogViewModel
+
 @Composable
 fun DialogTestScreen(viewModel: DialogViewModel = hiltViewModel()) {
-    val userInfos = viewModel.joinClubUserInfo.collectAsState()
+    val uiState = viewModel.dialogUiState.collectAsState()
     val isDialogOpen = remember { mutableStateListOf<Boolean>() }
 
+    when (uiState.value) {
+        is DialogState.Loading -> {
+            CircularProgressIndicator()
+        }
+
+        is DialogState.Success -> {
+            DialogTestContent(isOpen = isDialogOpen, dialogState = uiState.value as DialogState.Success)
+        }
+    }
+
+}
+
+@Composable
+private fun DialogTestContent(isOpen: MutableList<Boolean>, dialogState: DialogState.Success) {
+    val userInfos = remember {
+        dialogState.data.toMutableList()
+    }
     Column(modifier = Modifier.background(Color.White)) {
         Button(onClick = {
-            isDialogOpen.clear()
-            isDialogOpen.addAll(List(userInfos.value.size) { true })
+            isOpen.clear()
+            isOpen.addAll(List(userInfos.size) { true })
         }) {
             Text(text = "모든 다이얼로그 보기")
         }
 
-        userInfos.value.forEachIndexed { index, userInfo ->
-            if (isDialogOpen.getOrNull(index) == true) {
+        userInfos.forEachIndexed { index, userInfo ->
+            if (isOpen.getOrNull(index) == true) {
                 ClubJoinCheckDialog(
                     userInfo = userInfo,
-                    onDismiss = { isDialogOpen[index] = false }
+                    onDismiss = { isOpen[index] = false }
                 )
             }
         }
--- features/feature-dialog/src/main/java/com/example/feature_dialog/presentation/viewmodel/DialogViewModel.kt
+++ features/feature-dialog/src/main/java/com/example/feature_dialog/presentation/viewmodel/DialogViewModel.kt
@@ -2,8 +2,8 @@ package com.example.feature_dialog.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.core.model.UserInfo
 import com.example.feature_dialog.domain.GetJoinClubUserInfoUseCase
+import com.example.feature_dialog.presentation.DialogState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -14,8 +14,8 @@ import javax.inject.Inject
 class DialogViewModel @Inject constructor(
     private val getJoinClubUserInfoUseCase: GetJoinClubUserInfoUseCase
 ) : ViewModel() {
-    private val _joinClubUserInfo = MutableStateFlow<List<UserInfo>>(listOf( UserInfo("","","","","")))
-    val joinClubUserInfo: StateFlow<List<UserInfo>> = _joinClubUserInfo
+    private val _dialogUiState = MutableStateFlow<DialogState>(DialogState.Loading)
+    val dialogUiState: StateFlow<DialogState> = _dialogUiState
 
     init {
         getJoinClubUserInfo()
@@ -23,7 +23,7 @@ class DialogViewModel @Inject constructor(
 
     fun getJoinClubUserInfo() {
         viewModelScope.launch {
-            _joinClubUserInfo.value = getJoinClubUserInfoUseCase()
+            _dialogUiState.value = DialogState.Success(getJoinClubUserInfoUseCase())
         }
     }
 
