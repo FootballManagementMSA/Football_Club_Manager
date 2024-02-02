f5eb620e94f112aa4e2329b821da178430009075
lyh990517 <lyh990517@naver.com>
2024-02-02 23:14:57 +0900

    [Feature-Schedule] Separate UI Component

--- features/feature-schedule/src/main/java/com/example/feature_schedule/MakeScheduleScreen.kt
+++ features/feature-schedule/src/main/java/com/example/feature_schedule/MakeScheduleScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -31,6 +32,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.calendar.Calendar
+import com.example.ui_component.R
 import com.example.ui_component.buttons.CustomGradientButton
 import com.example.ui_component.template.DefaultBottomSheet
 import com.example.ui_component.template.DefaultEmblemSelectIconView
@@ -51,21 +53,7 @@ fun MakeScheduleScreen() {
     val showCalendar = remember { mutableStateOf(false) }
     val setStart = remember { mutableStateOf(false) }
     if (showCalendar.value) {
-        DefaultBottomSheet(onDismiss = { showCalendar.value = false }) {
-            Calendar(
-                modifier = Modifier
-                    .padding(20.dp)
-                    .height(350.dp)
-            ) {
-                if (setStart.value) {
-                    startDate.value = "${it.year}-${it.month}-${it.day}"
-                    setStart.value = false
-                } else {
-                    endDate.value = "${it.year}-${it.month}-${it.day}"
-                }
-                showCalendar.value = false
-            }
-        }
+        CalendarSheet(showCalendar, setStart, startDate, endDate)
     }
     Column(
         Modifier
@@ -74,119 +62,16 @@ fun MakeScheduleScreen() {
             .padding(20.dp)
     ) {
         Spacer(modifier = Modifier.weight(1f))
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Text(text = "일정 제목", color = Color.Gray)
-                DefaultEmblemSelectIconView(
-                    modifier = Modifier
-                        .clip(CircleShape)
-                        .background(emblem)
-                        .size(60.dp)
-                        .padding(top = 5.dp),
-                    state = uri,
-                    defaultIcon = com.example.ui_component.R.drawable.league_icon
-                ) {
-
-                }
-            }
-            InputView(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 10.dp),
-                text = { title.value },
-                onChange = { title.value = it }, placeholder = "일정 제목을 입력해주세요."
-            )
-        }
+        ScheduleTitleView(uri, title)
         Spacer(modifier = Modifier.weight(0.4f))
-        Text("일정 메모", color = Color.Gray)
-        InputView(
-            modifier = Modifier
-                .fillMaxWidth(),
-            text = { memo.value },
-            onChange = { memo.value = it }, placeholder = "메모를 입력해주세요."
-        )
+        ScheduleMemoView(memo)
         Spacer(modifier = Modifier.weight(0.4f))
-        Text(text = "일정 날짜", color = Color.Gray)
-        Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
-            InputView(
-                modifier = Modifier
-                    .weight(1f),
-                text = { startDate.value },
-                onChange = { startDate.value = it }, placeholder = "시작 날짜를 입력해주세요.",
-                leadingIcon = {
-                    Icon(
-                        modifier = Modifier.clickable {
-                            showCalendar.value = true
-                            setStart.value = true
-                        },
-                        imageVector = Icons.Default.DateRange,
-                        contentDescription = ""
-                    )
-                }
-            )
-            Box(
-                modifier = Modifier
-                    .width(10.dp)
-                    .height(1.dp)
-                    .background(Color.Gray)
-            )
-            InputView(
-                modifier = Modifier
-                    .weight(1f),
-                text = { endDate.value },
-                onChange = { endDate.value = it }, placeholder = "종료 날짜를 입력해주세요.",
-
-                leadingIcon = {
-                    Icon(
-                        modifier = Modifier.clickable {
-                            showCalendar.value = true
-                        },
-                        imageVector = Icons.Default.DateRange,
-                        contentDescription = ""
-                    )
-                }
-            )
-        }
+        ScheduleDateView(startDate, showCalendar, setStart, endDate)
         Spacer(modifier = Modifier.weight(0.4f))
-        Text("일정 장소", color = Color.Gray)
-        InputView(
-            modifier = Modifier
-                .fillMaxWidth(),
-            text = { location.value },
-            onChange = { location.value = it }, placeholder = "일정 장소를 입력해주세요."
-        )
+        ScheduleLocationView(location)
         Spacer(modifier = Modifier.weight(0.4f))
         Text("상대팀 성정", color = Color.Gray)
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            DefaultEmblemSelectIconView(
-                modifier = Modifier
-                    .clip(CircleShape)
-                    .background(emblem)
-                    .size(40.dp)
-                    .padding(top = 5.dp),
-                state = uri,
-                defaultIcon = com.example.ui_component.R.drawable.league_icon
-            ) {
-
-            }
-            Text(
-                modifier = Modifier.padding(horizontal = 10.dp),
-                text = "vs",
-                color = Color.Gray,
-                fontSize = middleFont
-            )
-            DefaultEmblemSelectIconView(
-                modifier = Modifier
-                    .clip(CircleShape)
-                    .background(emblem)
-                    .size(40.dp)
-                    .padding(top = 5.dp),
-                state = uri,
-                defaultIcon = com.example.ui_component.R.drawable.league_icon
-            ) {
-
-            }
-        }
+        SetTeamView(uri)
         Spacer(modifier = Modifier.weight(1f))
         CustomGradientButton(
             gradientColors = gradientColorsList,
@@ -197,6 +82,166 @@ fun MakeScheduleScreen() {
 
         }
         Spacer(modifier = Modifier.weight(1f))
+    }
+}
+
+@Composable
+private fun SetTeamView(uri: MutableState<Uri?>) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        DefaultEmblemSelectIconView(
+            modifier = Modifier
+                .clip(CircleShape)
+                .background(emblem)
+                .size(40.dp)
+                .padding(top = 5.dp),
+            state = uri,
+            defaultIcon = R.drawable.league_icon
+        ) {
+
+        }
+        Text(
+            modifier = Modifier.padding(horizontal = 10.dp),
+            text = "vs",
+            color = Color.Gray,
+            fontSize = middleFont
+        )
+        DefaultEmblemSelectIconView(
+            modifier = Modifier
+                .clip(CircleShape)
+                .background(emblem)
+                .size(40.dp)
+                .padding(top = 5.dp),
+            state = uri,
+            defaultIcon = R.drawable.league_icon
+        ) {
+
+        }
+    }
+}
+
+@Composable
+private fun ScheduleLocationView(location: MutableState<String>) {
+    Text("일정 장소", color = Color.Gray)
+    InputView(
+        modifier = Modifier
+            .fillMaxWidth(),
+        text = { location.value },
+        onChange = { location.value = it }, placeholder = "일정 장소를 입력해주세요."
+    )
+}
+
+@Composable
+private fun ScheduleDateView(
+    startDate: MutableState<String>,
+    showCalendar: MutableState<Boolean>,
+    setStart: MutableState<Boolean>,
+    endDate: MutableState<String>
+) {
+    Text(text = "일정 날짜", color = Color.Gray)
+    Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+        InputView(
+            modifier = Modifier
+                .weight(1f),
+            text = { startDate.value },
+            onChange = { startDate.value = it }, placeholder = "시작 날짜를 입력해주세요.",
+            leadingIcon = {
+                Icon(
+                    modifier = Modifier.clickable {
+                        showCalendar.value = true
+                        setStart.value = true
+                    },
+                    imageVector = Icons.Default.DateRange,
+                    contentDescription = ""
+                )
+            }
+        )
+        Box(
+            modifier = Modifier
+                .width(10.dp)
+                .height(1.dp)
+                .background(Color.Gray)
+        )
+        InputView(
+            modifier = Modifier
+                .weight(1f),
+            text = { endDate.value },
+            onChange = { endDate.value = it }, placeholder = "종료 날짜를 입력해주세요.",
+
+            leadingIcon = {
+                Icon(
+                    modifier = Modifier.clickable {
+                        showCalendar.value = true
+                    },
+                    imageVector = Icons.Default.DateRange,
+                    contentDescription = ""
+                )
+            }
+        )
+    }
+}
+
+@Composable
+private fun ScheduleMemoView(memo: MutableState<String>) {
+    Text("일정 메모", color = Color.Gray)
+    InputView(
+        modifier = Modifier
+            .fillMaxWidth(),
+        text = { memo.value },
+        onChange = { memo.value = it }, placeholder = "메모를 입력해주세요."
+    )
+}
+
+@Composable
+private fun ScheduleTitleView(
+    uri: MutableState<Uri?>,
+    title: MutableState<String>
+) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "일정 제목", color = Color.Gray)
+            DefaultEmblemSelectIconView(
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .background(emblem)
+                    .size(60.dp)
+                    .padding(top = 5.dp),
+                state = uri,
+                defaultIcon = R.drawable.league_icon
+            ) {
+
+            }
+        }
+        InputView(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 10.dp),
+            text = { title.value },
+            onChange = { title.value = it }, placeholder = "일정 제목을 입력해주세요."
+        )
+    }
+}
+
+@Composable
+private fun CalendarSheet(
+    showCalendar: MutableState<Boolean>,
+    setStart: MutableState<Boolean>,
+    startDate: MutableState<String>,
+    endDate: MutableState<String>
+) {
+    DefaultBottomSheet(onDismiss = { showCalendar.value = false }) {
+        Calendar(
+            modifier = Modifier
+                .padding(20.dp)
+                .height(350.dp)
+        ) {
+            if (setStart.value) {
+                startDate.value = "${it.year}-${it.month}-${it.day}"
+                setStart.value = false
+            } else {
+                endDate.value = "${it.year}-${it.month}-${it.day}"
+            }
+            showCalendar.value = false
+        }
     }
 }
 
