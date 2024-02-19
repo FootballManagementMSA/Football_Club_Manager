8cc69a2ddcb6d8d1f6d6f2a9d206835eb9ea23fa
임성우 <seongwoorang@naver.com>
2024-02-19 17:57:58 +0900

    [Feature-Login] Add error mapper

---
+++ libraries/network-api/src/main/java/com/example/network_api/ErrorType.kt
@@ -0,0 +1,6 @@
+package com.example.network_api
+
+data class ErrorType(
+    val errorMessage: String,
+    val code: String?
+)
--- libraries/network-api/src/main/java/com/example/network_api/response/RespResult.kt
+++ libraries/network-api/src/main/java/com/example/network_api/response/RespResult.kt
@@ -1,6 +1,8 @@
 package com.example.network_api.response
 
+import com.example.network_api.ErrorType
+
 sealed class RespResult<T> {
     data class Success<T>(val data: T) : RespResult<T>()
-    data class Error<T>(val error: String) : RespResult<T>()
+    data class Error<T>(val error: ErrorType) : RespResult<T>()
 }
