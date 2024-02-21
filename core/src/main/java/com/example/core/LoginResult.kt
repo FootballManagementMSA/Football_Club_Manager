f3897d864544f832de704a54489e0a4fbd100b59
임성우 <seongwoorang@naver.com>
2024-02-21 15:03:47 +0900

    [Feature-Login] Add Uninitialize State In LoginResult

--- core/src/main/java/com/example/core/LoginResult.kt
+++ core/src/main/java/com/example/core/LoginResult.kt
@@ -3,4 +3,5 @@ package com.example.core
 sealed class LoginResult {
     data class Success(val accessToken: String, val refreshToken: String) : LoginResult()
     data class Error(val errorMessage: String) : LoginResult()
+    data object UnInitialize : LoginResult()
 }
