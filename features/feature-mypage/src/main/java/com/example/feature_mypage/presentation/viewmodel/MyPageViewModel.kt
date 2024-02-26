ee467c79260b62443edefb1ae919d23d93275fc6
임성우 <seongwoorang@naver.com>
2024-02-26 18:13:38 +0900

    [Feature-MyPage] Initialize UserInfo At ViewModel

--- features/feature-mypage/src/main/java/com/example/feature_mypage/presentation/viewmodel/MyPageViewModel.kt
+++ features/feature-mypage/src/main/java/com/example/feature_mypage/presentation/viewmodel/MyPageViewModel.kt
@@ -2,21 +2,34 @@ package com.example.feature_mypage.presentation.viewmodel
 
 import android.net.Uri
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.feature_mypage.domain.usecase.GetUserInfoUseCase
+import com.example.feature_mypage.presentation.UserInfoState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MyPageViewModel @Inject constructor(
+    private val getUserInfoUseCase: GetUserInfoUseCase
 ): ViewModel() {
     private  val _selectedImageUri = MutableStateFlow<Uri?>(null)
     val selectedImageUri: StateFlow<Uri?> = _selectedImageUri
 
+    private val _uiState = MutableStateFlow<UserInfoState>(UserInfoState.Loading)
+    val uiState get() = _uiState
     init {
-        // 서버에서 이미지 가져오기
+        loadUserInfo()
     }
     fun updateSelectedImageUri(uri: Uri?) {
         _selectedImageUri.value = uri
+    }
+
+    private fun loadUserInfo() {
+        viewModelScope.launch {
+            _uiState.value = UserInfoState.Success(getUserInfoUseCase())
+        }
     }
 }
