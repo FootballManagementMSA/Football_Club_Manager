87e7ec8607bbe2a9f5749670de8c343a8433ed93
임성우 <seongwoorang@naver.com>
2024-02-26 18:06:24 +0900

    [Feature-MyPage] Add getUserInfo Func In UserRemoteDataSource

--- core/src/main/java/com/example/core/datasource/UserRemoteDataSource.kt
+++ core/src/main/java/com/example/core/datasource/UserRemoteDataSource.kt
@@ -2,10 +2,12 @@ package com.example.core.datasource
 
 import com.example.core.LoginResult
 import com.example.core.model.LoginModel
+import com.example.core.model.MyPageUserInfoUiModel
 import com.example.core.model.UserInfo
 
 interface UserRemoteDataSource {
     suspend fun login(loginModel: LoginModel): LoginResult
     suspend fun sendClubInfoData()
     suspend fun getJoinClubUserInfo(): List<UserInfo>
+    suspend fun getUserInfo(): MyPageUserInfoUiModel
 }
