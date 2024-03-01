7c41942909eab5840c1d6f19ad64a0157c2aa4ff
임성우 <seongwoorang@naver.com>
2024-03-01 20:58:23 +0900

    [Core] Add Result State

--- core/src/main/java/com/example/core/ResultState/LoginResult.kt
+++ core/src/main/java/com/example/core/ResultState/LoginResult.kt
@@ -1,4 +1,4 @@
-package com.example.core
+package com.example.core.ResultState
 
 sealed class LoginResult {
     data class Success(val accessToken: String, val refreshToken: String) : LoginResult()
