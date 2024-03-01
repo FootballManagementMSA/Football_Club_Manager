89abc17510794162a283a69e1f96689421158f5e
임성우 <seongwoorang@naver.com>
2024-03-01 21:07:01 +0900

    [Feature-MyPage] Add ModifyUserInfo At Repository

--- libraries/network-api/src/main/java/com/example/network_api/repository/UserRepository.kt
+++ libraries/network-api/src/main/java/com/example/network_api/repository/UserRepository.kt
@@ -2,10 +2,22 @@ package com.example.network_api.repository
 
 import com.example.network_api.entity.Login
 import com.example.network_api.response.LoginResponse
+import com.example.network_api.response.ModifyUserInfoResponse
 import com.example.network_api.response.RespResult
 import com.example.network_api.response.UserInfoResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 
 interface UserRepository {
     suspend fun login(loginReq: Login): RespResult<LoginResponse>
     suspend fun getUserInfo(): RespResult<UserInfoResponse>
+    suspend fun modifyUserInfo(
+        name: RequestBody,
+        age: RequestBody,
+        height: RequestBody,
+        sex: RequestBody,
+        position: RequestBody,
+        foot: RequestBody,
+        image: MultipartBody.Part
+    ): RespResult<ModifyUserInfoResponse>
 }
