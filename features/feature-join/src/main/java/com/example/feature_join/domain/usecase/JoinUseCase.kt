da852b608bb3f46cc37662eb0007d4a4ae409239
chanhue <chanhue467@gmail.com>
2024-03-02 12:07:30 +0900

    [feature-join] modifiy JoinUseCase

--- features/feature-join/src/main/java/com/example/feature_join/domain/usecase/JoinUseCase.kt
+++ features/feature-join/src/main/java/com/example/feature_join/domain/usecase/JoinUseCase.kt
@@ -1,10 +1,25 @@
 package com.example.feature_join.domain.usecase
 
+import com.example.core.JoinResult
+import com.example.core.LoginResult
 import com.example.core.datasource.UserLocalDataSource
+import com.example.core.datasource.UserRemoteDataSource
+import com.example.core.model.JoinModel
+import com.example.core.model.LoginModel
 import javax.inject.Inject
 
 class JoinUseCase @Inject constructor(
-    private val userLocalDataSource: UserLocalDataSource
+    private val userRemoteDataSource: UserRemoteDataSource
 ) {
-    suspend operator fun invoke() = userLocalDataSource.join()
+    suspend operator fun invoke(joinModel: JoinModel) :JoinResult{
+        return userRemoteDataSource.join(joinModel)
+    }
 }
+/*
+class LoginUseCase @Inject constructor(
+    private val userRemoteDataSource: UserRemoteDataSource
+) {
+    suspend operator fun invoke(loginModel: LoginModel): LoginResult {
+        return userRemoteDataSource.login(loginModel)
+    }
+}*/
