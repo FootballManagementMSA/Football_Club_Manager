549612333b252171991d8c6746c678bcfd24454b
chanhue <chanhue467@gmail.com>
2024-03-05 22:52:15 +0900

    [feature-join] Add Join Api

---
+++ libraries/network-api/src/main/java/com/example/network_api/entity/Join.kt
@@ -0,0 +1,13 @@
+package com.example.network_api.entity
+
+data class Join(
+
+    val studentId:String,
+    val password:String,
+    val position:String,
+    val foot:String,
+    val sex:String,
+    val age:Int,
+    val height:Int
+)
+
