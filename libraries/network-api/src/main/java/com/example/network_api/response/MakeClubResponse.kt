2657594f4a30f885f377580689787026e0d613f8
임성우 <seongwoorang@naver.com>
2024-03-06 16:43:23 +0900

    [Feature-ConnectMakeClub] Add Response Model

---
+++ libraries/network-api/src/main/java/com/example/network_api/response/MakeClubResponse.kt
@@ -0,0 +1,14 @@
+package com.example.network_api.response
+
+import com.google.gson.annotations.SerializedName
+
+data class MakeClubResponse(
+    val status: Int,
+    val message: String,
+    @SerializedName("data")
+    val unique: UniqueNumberData
+)
+
+data class UniqueNumberData(
+   val uniqueNumber: String
+)
