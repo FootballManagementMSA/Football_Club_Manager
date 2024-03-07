0e69c2ffdb837e0e8662132fad386239237e1e1d
chanhue <chanhue467@gmail.com>
2024-03-07 21:38:14 +0900

    [feature-join] delete import value

--- features/feature-join/src/main/java/com/example/feature_join/presentation/ui_component/GenderView.kt
+++ features/feature-join/src/main/java/com/example/feature_join/presentation/ui_component/GenderView.kt
@@ -10,9 +10,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun GenderView(onGenderSelected: (String) -> Unit) {
