c8d280bda2217f227a675f69db779f7676a77821
임성우 <seongwoorang@naver.com>
2024-03-08 23:05:34 +0900

    [Feature-JoinClubResult] Modify Data Class Content

--- core/src/main/java/com/example/core/model/Club.kt
+++ core/src/main/java/com/example/core/model/Club.kt
@@ -1,7 +1,19 @@
 package com.example.core.model
 
 data class Club(
+    val status: Int,
+    val code: String,
+    val message: String,
+    val data: ClubData,
+)
+
+data class ClubData(
+    val team: List<ClubInfo>
+)
+
+data class ClubInfo(
+    val id: String,
     val name: String,
-    val person: String,
-    val introduction: String
+    val memberNum: String,
+    val star: Int
 )
