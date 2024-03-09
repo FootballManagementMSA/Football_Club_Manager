99c2613412df8ae7525d705e39b553e0bca40d18
임성우 <seongwoorang@naver.com>
2024-03-09 22:56:25 +0900

    [Feature-JoinClubResult] Add ViewModel

--- features/feature-joinclub/src/main/java/com/example/feature_joinclub/presentation/viewmodel/ClubSearchViewModel.kt
+++ features/feature-joinclub/src/main/java/com/example/feature_joinclub/presentation/viewmodel/ClubSearchViewModel.kt
@@ -2,7 +2,10 @@ package com.example.feature_joinclub.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.core.model.ClubInfo
+import com.example.feature_joinclub.domain.usecase.SearchClubUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -10,15 +13,20 @@ import javax.inject.Inject
 
 @HiltViewModel
 class ClubSearchViewModel @Inject constructor(
-
+    private val searchClubUseCase: SearchClubUseCase
 ): ViewModel(){
+    private val tempDispatcher = Dispatchers.IO
+
     private val _searchValue = MutableStateFlow("")
     val searchValue: StateFlow<String> =_searchValue
 
+    private val _searchedClub = MutableStateFlow<List<ClubInfo>>(listOf())
+    val searchedClub: StateFlow<List<ClubInfo>> = _searchedClub
+
     fun searchClub(clubName: String) {
         _searchValue.value = clubName
-        viewModelScope.launch {
-
+        viewModelScope.launch(tempDispatcher) {
+            _searchedClub.value = searchClubUseCase(clubName)
         }
     }
 
