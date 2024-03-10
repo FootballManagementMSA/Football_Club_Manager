ffcd6ddeb3cc0be173391b524ac629cbd7d7bd6e
임성우 <seongwoorang@naver.com>
2024-03-10 15:01:16 +0900

    [Feature-JoinClubResult] Modify Model Content

--- core/src/main/java/com/example/core/model/Club.kt
+++ core/src/main/java/com/example/core/model/Club.kt
@@ -2,14 +2,14 @@ package com.example.core.model
 
 data class Club(
     val status: Int,
-    val code: String,
     val message: String,
     val data: List<ClubInfo>,
 )
 
 data class ClubInfo(
-    val id: String,
-    val name: String,
-    val memberNum: String,
-    val star: Int
+    val teamId: Int,
+    val teamName: String,
+    val totalMemberCnt: Int,
+    val uniqueNum: String,
+    val emblem: String
 )
--- libraries/network-api/src/main/java/com/example/network_api/response/SearchClubResponse.kt
+++ libraries/network-api/src/main/java/com/example/network_api/response/SearchClubResponse.kt
@@ -2,14 +2,14 @@ package com.example.network_api.response
 
 data class SearchClubResponse(
     val status: Int,
-    val code: String,
     val message: String,
     val data: List<ClubInfoResponse>,
 )
 
 data class ClubInfoResponse(
-    val id: String,
-    val name: String,
-    val memberNum: String,
-    val star: Int
+    val teamId: Int,
+    val teamName: String,
+    val totalMemberCnt: Int,
+    val uniqueNum: String,
+    val emblem: String
 )
