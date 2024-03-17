837a1c5a01efd96d5ced9c594c3d8ca0d697ba58
임성우 <seongwoorang@naver.com>
2024-03-17 20:22:31 +0900

    [Feature-MainHomeApi] Add UseCase

--- features/feature-home/src/main/java/com/example/domain/usecase/LoadStudentDataUseCase.kt
+++ features/feature-home/src/main/java/com/example/domain/usecase/LoadStudentDataUseCase.kt
@@ -3,8 +3,8 @@ package com.example.domain.usecase
 import com.example.core.datasource.MainHomeDataSource
 import javax.inject.Inject
 
-class LoadDataUseCase @Inject constructor(
+class LoadStudentDataUseCase @Inject constructor(
     private val mainHomeDataSource: MainHomeDataSource
 ) {
-    suspend operator fun invoke() = mainHomeDataSource.loadData()
+    suspend operator fun invoke() = mainHomeDataSource.loadStudentData().data
 }
