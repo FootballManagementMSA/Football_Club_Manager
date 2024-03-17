fdcd24dc8587420428e12aa462326ba304d25b66
임성우 <seongwoorang@naver.com>
2024-03-17 20:25:53 +0900

    [Feature-MainHomeApi] Add ViewModel

--- features/feature-home/src/main/java/com/example/presentation/viewmodel/MainHomeViewModel.kt
+++ features/feature-home/src/main/java/com/example/presentation/viewmodel/MainHomeViewModel.kt
@@ -1,9 +1,9 @@
 package com.example.presentation.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.domain.usecase.LoadDataUseCase
+import com.example.domain.usecase.LoadStudentDataUseCase
+import com.example.presentation.StudentDataState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -13,14 +13,13 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainHomeViewModel @Inject constructor(
-    private val loadDataUseCase: LoadDataUseCase
+    private val loadStudentDataUseCase: LoadStudentDataUseCase
 ): ViewModel() {
     private val tempDispatcher = Dispatchers.IO
-    private val _uiState = MutableStateFlow("")
+    private val _uiState = MutableStateFlow<StudentDataState>(StudentDataState.Loading)
     val uiState get() = _uiState.asStateFlow()
+
     fun getResponse() = viewModelScope.launch(tempDispatcher) {
-        _uiState.value = ""
-        val a = loadDataUseCase.invoke()
-        Log.e("123123","$a")
+        _uiState.value = StudentDataState.Success(loadStudentDataUseCase())
     }
 }
