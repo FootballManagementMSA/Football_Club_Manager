27a185d41d801873780cf52d0b36f58535376b11
임성우 <seongwoorang@naver.com>
2024-03-17 22:54:26 +0900

    [Feature-MainHomeApi] Modify Func Name

--- core/src/main/java/com/example/core/datasource/MainHomeDataSource.kt
+++ core/src/main/java/com/example/core/datasource/MainHomeDataSource.kt
@@ -1,7 +1,9 @@
 package com.example.core.datasource
 
+import com.example.core.model.MainHomeScheduleUiModel
 import com.example.core.model.MainHomeStudentDataUiModel
 
 interface MainHomeDataSource {
     suspend fun loadStudentData() : MainHomeStudentDataUiModel
+    suspend fun loadSchedule() : MainHomeScheduleUiModel
 }
--- features/feature-home/src/main/java/com/example/domain/usecase/LoadSchedueUseCase.kt
+++ features/feature-home/src/main/java/com/example/domain/usecase/LoadSchedueUseCase.kt
@@ -6,5 +6,5 @@ import javax.inject.Inject
 class LoadSchedueUseCase @Inject constructor(
     private val mainHomeDataSource: MainHomeDataSource
 ){
-    suspend operator fun invoke() = mainHomeDataSource.loadStudentData().data
+    suspend operator fun invoke() = mainHomeDataSource.loadSchedule().data.schedule
 }
