01a76591c08ff34de588f623d7cbb2f378ef3cef
chanhue <chanhue467@gmail.com>
2024-03-28 01:39:56 +0900

    [feature-clubpage] add Clubapi

--- libraries/network-api/src/main/java/com/example/network_api/api/ClubApi.kt
+++ libraries/network-api/src/main/java/com/example/network_api/api/ClubApi.kt
@@ -1,14 +1,20 @@
 package com.example.network_api.api
 
+import com.example.network_api.entity.ClubJoin
+import com.example.network_api.entity.Join
+import com.example.network_api.response.ClubJoinResponse
+import com.example.network_api.response.JoinResponse
 import com.example.network_api.response.MakeClubResponse
 import com.example.network_api.response.SearchClubResponse
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ClubApi {
@@ -23,4 +29,23 @@ interface ClubApi {
     suspend fun searchClub(
         @Query("search") code: String
     ): Response<SearchClubResponse>
+
+
+    @POST("/api/team-service/teams/52/apply")
+    suspend fun clubJoin(
+        @Body JoinReq: ClubJoin
+    ):Response<ClubJoinResponse>
+    /*
+    @POST("/api/team-service/teams/{teamId}/apply")
+    suspend fun clubJoin(
+        @Path("teamId") teamId:String,
+        @Body JoinReq: ClubJoin
+    ):Response<ClubJoinResponse>
+    */
+
+
+
+
+
+
 }
