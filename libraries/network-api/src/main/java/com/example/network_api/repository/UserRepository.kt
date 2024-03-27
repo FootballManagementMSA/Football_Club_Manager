483f886ad9da3cff05b6a64ba5f0377479695202
chanhue <chanhue467@gmail.com>
2024-03-28 01:36:51 +0900

    [feature-clubpage] add user repository

--- libraries/network-api/src/main/java/com/example/network_api/repository/UserRepository.kt
+++ libraries/network-api/src/main/java/com/example/network_api/repository/UserRepository.kt
@@ -1,7 +1,9 @@
 package com.example.network_api.repository
 
+import com.example.network_api.entity.ClubJoin
 import com.example.network_api.entity.Join
 import com.example.network_api.entity.Login
+import com.example.network_api.response.ClubJoinResponse
 import com.example.network_api.response.JoinResponse
 import com.example.network_api.response.LoginResponse
 import com.example.network_api.response.ModifyUserInfoResponse
@@ -13,6 +15,8 @@ import okhttp3.RequestBody
 interface UserRepository {
     suspend fun login(loginReq: Login): RespResult<LoginResponse>
     suspend fun join(joinReq: Join): RespResult<JoinResponse>
+
+    suspend fun clubJoinRequest(clubJoinReq: ClubJoin):RespResult<ClubJoinResponse>
 
 
     suspend fun getUserInfo(): RespResult<UserInfoResponse>
--- libraries/network/src/main/java/com/example/network/repository/UserRepositoryImpl.kt
+++ libraries/network/src/main/java/com/example/network/repository/UserRepositoryImpl.kt
@@ -2,10 +2,13 @@ package com.example.network.repository
 
 import com.example.network_api.ErrorType
 import com.example.network_api.RespMapper
+import com.example.network_api.api.ClubApi
 import com.example.network_api.api.FootballManagerApi
+import com.example.network_api.entity.ClubJoin
 import com.example.network_api.entity.Join
 import com.example.network_api.entity.Login
 import com.example.network_api.repository.UserRepository
+import com.example.network_api.response.ClubJoinResponse
 import com.example.network_api.response.JoinResponse
 import com.example.network_api.response.LoginResponse
 import com.example.network_api.response.ModifyUserInfoResponse
@@ -16,7 +19,8 @@ import okhttp3.RequestBody
 import javax.inject.Inject
 
 internal class UserRepositoryImpl @Inject constructor(
-    private val footballManagerApi: FootballManagerApi
+    private val footballManagerApi: FootballManagerApi,
+    private val clubApi: ClubApi
 ) : UserRepository {
     override suspend fun login(loginReq: Login): RespResult<LoginResponse> {
         val response = footballManagerApi.login(loginReq)
@@ -79,4 +83,15 @@ internal class UserRepositoryImpl @Inject constructor(
             val errorBody = RespMapper.errorMapper(errorBodyJson)
             RespResult.Error(ErrorType(errorBody.message!!, errorBody.code))
         }    }
+
+    override suspend fun clubJoinRequest(clubJoinReq: ClubJoin): RespResult<ClubJoinResponse> {
+        val response = clubApi.clubJoin(clubJoinReq)
+
+        return if (response.isSuccessful) {
+            RespResult.Success(response.body()!!)
+        } else {
+            val errorBodyJson = response.errorBody()?.string() ?: ""
+            val errorBody = RespMapper.errorMapper(errorBodyJson)
+            RespResult.Error(ErrorType(errorBody.message!!, errorBody.code))
+        }      }
 }
